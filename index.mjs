616a2af6e85c86f571345a6d9c24f3286977160d
atoris1192 <atoris1192@gmail.com>
2022-01-25 10:24:35 +0900

    view add

--- index.mjs
+++ index.mjs
@@ -1,43 +1,12 @@
 
-import * as path from "path"
+// import * as path from "path"
 import  Nedb  from "nedb";
 
-const filepath = path.join(__dirname, 'sample1.db');
+// const filepath = path.join(__dirname, 'sample1.db');
 const db = new Nedb({
-    filename: filepath,
+    // filename: 'todo.db',
     autoload: true,
 });
-
-// var doc = { hello: 'world'
-//                , n: 5
-//                , today: new Date()
-//                , nedbIsAwesome: true
-//                , notthere: null
-//                , notToBeSaved: undefined  // Will not be saved
-//                , fruits: [ 'apple', 'orange', 'pear' ]
-//                , infos: { name: 'nedb' }
-//                };
-
-// db.insert(doc, function (err, newDoc) {   // Callback is optional
-//   if(err) {console.error('Err: ',err)}
-//   console.log('newDoc: ',newDoc)
-// });
-
-const getAll= () => {
-  return new Promise((resolve, reject) => {
-    db.find({}, (err, data)=> {
-      if (err) { reject(err)}
-      else resolve(data)
-    })
-  })
-  .catch(err => { console.error(err)})
-};
-
-getAll()
-  .then(res => {
-    console.log(res)
-  })
-
 
 
 const formElement = document.querySelector("#js-form");
@@ -46,7 +15,7 @@ const containerElement = document.querySelector("#js-todo-list");
 const todoItemCountElement = document.querySelector("#js-todo-count");
 
 
-let todoListItems = [];
+// let todoListItems = [];
 let idNum = 0;
 
 formElement.addEventListener("submit" , (event) => {
@@ -68,5 +37,52 @@ formElement.addEventListener("submit" , (event) => {
 
     // console.log(todoListItems)
 
+    // view関係
+    const ulElement = htmlToElement(`<ul />`)
+    getAll()
+      .then(res => {
+        res.forEach((ITEM) => {
+            const liElement = htmlToElement(`<li>${ITEM.title}</li>`)
+            ulElement.appendChild(liElement)
+        })
+        // console.log(res)
+      })
+    containerElement.innerHTML = "";
+    containerElement.appendChild(ulElement);
+
+    getCount()
+      .then(count => { 
+        // console.log(count)
+        todoItemCountElement.textContent = `Todoアイテム数 : ${count}`
+      })
+
+
 })
 
+
+function htmlToElement(html) {
+    const template = document.createElement("template");
+    template.innerHTML = html;
+    return template.content.firstElementChild;
+}
+
+
+const getAll= () => {
+  return new Promise((resolve, reject) => {
+    db.find({}, (err, data)=> {
+      if (err) { reject(err)}
+      else resolve(data)
+    })
+  })
+  .catch(err => { console.error(err)})
+};
+
+const getCount = () => {
+  return new Promise((resolve, reject) => {
+    db.count({}, (err, count) => {
+      if (err) {reject(err)}
+      else resolve(count)
+    })
+  })
+  .catch(err => { console.error(err)})
+}
